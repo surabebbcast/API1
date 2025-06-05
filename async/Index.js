e729b40c0002059c571fa8a430976b94ede2477c
Surabi Belem <suravicastillomuniz@gmail.com>
2025-06-05 01:28:03 -0600

    CAMBIO1

---
+++ async/Index.js
@@ -0,0 +1,21 @@
+function soyUnCallbck(data){
+
+console.log('soy un callback ahora me toma a mi');
+console.log(data);
+
+}
+
+function haciendoUnaPeticionHttp(callback){
+console.log('Estoy haciendo otras operaciones ');
+console.log('Ya casi termino ');
+
+
+callback({
+    id:1,
+    username:'surabi belem',
+    edad:27
+})
+
+}
+
+haciendoUnaPeticionHttp(soyUnCallbck)
